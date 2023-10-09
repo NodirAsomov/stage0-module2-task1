2b0bf9e989294c0297fe3c2cbeb6a566c49d0d47
Нодир Асомов <nasomov93@gmail.com>
2023-10-09 05:42:55 +0500

    firstapp

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,7 +1,9 @@
 package com.mjc.stage0;
 
 public class FirstApplication {
+
+
     public static void main(String[] args) {
-        System.out.println(" Hello, World! ");
+        System.out.println("Hello, World!");
     }
 }
